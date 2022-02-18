497d899924b8641897e8b108488ed857fd2d14d2
Vis.St.PGEE@outlook.com <Vis.St.PGEE@outlook.com>
2022-02-18 10:38:28 +0200

    initial commit

---
+++ Recepti_12b_ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recepti_12b_ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
